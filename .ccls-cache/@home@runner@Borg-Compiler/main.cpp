0560750ef657e8208048615a48c0423811ab4632
7ayd <86687700+7ayd@users.noreply.github.com>
2022-05-15 00:59:14 +0000

    started

--- .ccls-cache/@home@runner@Borg-Compiler/main.cpp
+++ .ccls-cache/@home@runner@Borg-Compiler/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <list>
+#include <cstring>
+using namespace std;
+
+class HashTable {
+  private:
+    static const int hashgroup = 10; 
+    list <pair<int, string>> table[hashgroup]; 
+};
 
 int main() {
   std::cout << "Hello World!\n";
